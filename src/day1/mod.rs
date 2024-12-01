1ee9e905c614737d95acd6f103362fd61cd977ae
Matthew Walton <matthew@mathw.me.uk>
2024-12-01 11:07:10 +0000

    Day 1, Part 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,15 +1,18 @@
-use std::{error::Error, str::FromStr};
+use std::{collections::HashMap, error::Error, str::FromStr};
 
 pub fn run() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../inputs/day1.txt");
 
     let part1 = part1(input)?;
-
     println!("Part 1: Combined distance {}", part1);
+
+    let part2 = part2(input)?;
+    println!("Part 2: Similarity score {}", part2);
+
     Ok(())
 }
 
-fn parse_pairs(input: &str) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
+fn parse_lists(input: &str) -> Result<(Vec<u32>, Vec<u32>), Box<dyn Error>> {
     let pairs = input
         .lines()
         .map(|l| -> Result<(&str, &str), String> {
@@ -26,17 +29,23 @@ fn parse_pairs(input: &str) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
         })
         .collect::<Result<Vec<_>, _>>()?;
 
-    let mut first = pairs
+    let first = pairs
         .iter()
         .map(|p| u32::from_str(p.0))
         .collect::<Result<Vec<u32>, _>>()?;
 
-    first.sort();
-
-    let mut second = pairs
+    let second = pairs
         .iter()
         .map(|p| u32::from_str(p.1))
         .collect::<Result<Vec<u32>, _>>()?;
+
+    Ok((first, second))
+}
+
+fn parse_pairs(input: &str) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
+    let (mut first, mut second) = parse_lists(input)?;
+
+    first.sort();
 
     second.sort();
 
@@ -54,6 +63,28 @@ fn pair_distance(pair: &(u32, u32)) -> u32 {
 fn part1(input: &str) -> Result<u32, Box<dyn Error>> {
     let pairs = parse_pairs(input)?;
     Ok(pairs.iter().map(pair_distance).sum())
+}
+
+fn occurrance_map(i: &[u32]) -> HashMap<u32, u32> {
+    let mut map = HashMap::new();
+
+    for n in i.iter() {
+        let times_in_list = i.iter().filter(|x| *x == n).count() as u32;
+        map.insert(*n, times_in_list);
+    }
+
+    map
+}
+
+fn part2(input: &str) -> Result<u32, Box<dyn Error>> {
+    let (left, right) = parse_lists(input)?;
+    let occ_map = occurrance_map(&right);
+
+    let similarity_score = left
+        .into_iter()
+        .map(|l| l * occ_map.get(&l).unwrap_or(&0))
+        .sum();
+    Ok(similarity_score)
 }
 
 #[test]
@@ -78,4 +109,26 @@ fn test_part1() {
 3   3";
     let answer = part1(input).expect("No errors here please");
     assert_eq!(answer, 11);
+}
+
+#[test]
+fn test_occurrance_map() {
+    let input = vec![1, 2, 2, 3, 3, 3];
+    let map = occurrance_map(&input);
+    assert_eq!(map.len(), 3);
+    assert_eq!(map.get(&1), Some(&1));
+    assert_eq!(map.get(&2), Some(&2));
+    assert_eq!(map.get(&3), Some(&3));
+}
+
+#[test]
+fn test_part2() {
+    let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    assert_eq!(part2(input).expect("No errors wanted"), 31);
 }
