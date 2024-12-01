909494841e5e902d5dc4596a9dca536eada9b9d5
Matthew Walton <matthew@mathw.me.uk>
2024-12-01 11:27:08 +0000

    Day 1 make the parsing code nicer

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,12 +1,16 @@
 use std::{collections::HashMap, error::Error, str::FromStr};
 
+// https://adventofcode.com/2024/day/1
+
 pub fn run() -> Result<(), Box<dyn Error>> {
     let input = include_str!("../inputs/day1.txt");
 
-    let part1 = part1(input)?;
-    println!("Part 1: Combined distance {}", part1);
+    let (first, second) = parse_lists(input)?;
 
-    let part2 = part2(input)?;
+    let part2 = part2(&first, &second)?;
+    let part1 = part1(first, second)?;
+
+    println!("Part 1: Combined distance {}", part1);
     println!("Part 2: Similarity score {}", part2);
 
     Ok(())
@@ -15,38 +19,28 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 fn parse_lists(input: &str) -> Result<(Vec<u32>, Vec<u32>), Box<dyn Error>> {
     let pairs = input
         .lines()
-        .map(|l| -> Result<(&str, &str), String> {
+        .map(|l| {
             let mut parts = l.split_whitespace();
             if let Some(first) = parts.next() {
                 if let Some(second) = parts.next() {
-                    Ok((first, second))
+                    Ok((u32::from_str(first)?, u32::from_str(second)?))
                 } else {
-                    Err(format!("Line {} didn't have a second part", l))
+                    Err(format!("Line {} didn't have a second part", l).into())
                 }
             } else {
-                Err(format!("Line {} didn't have a first part", l))
+                Err(format!("Line {} didn't have a first part", l).into())
             }
         })
-        .collect::<Result<Vec<_>, _>>()?;
+        .collect::<Result<Vec<(u32, u32)>, Box<dyn Error>>>()?;
 
-    let first = pairs
-        .iter()
-        .map(|p| u32::from_str(p.0))
-        .collect::<Result<Vec<u32>, _>>()?;
-
-    let second = pairs
-        .iter()
-        .map(|p| u32::from_str(p.1))
-        .collect::<Result<Vec<u32>, _>>()?;
-
-    Ok((first, second))
+    Ok(pairs.into_iter().unzip())
 }
 
-fn parse_pairs(input: &str) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
-    let (mut first, mut second) = parse_lists(input)?;
-
+fn make_sorted_pairs(
+    mut first: Vec<u32>,
+    mut second: Vec<u32>,
+) -> Result<Vec<(u32, u32)>, Box<dyn Error>> {
     first.sort();
-
     second.sort();
 
     Ok(first.into_iter().zip(second).collect())
@@ -60,8 +54,8 @@ fn pair_distance(pair: &(u32, u32)) -> u32 {
     }
 }
 
-fn part1(input: &str) -> Result<u32, Box<dyn Error>> {
-    let pairs = parse_pairs(input)?;
+fn part1(first: Vec<u32>, second: Vec<u32>) -> Result<u32, Box<dyn Error>> {
+    let pairs = make_sorted_pairs(first, second)?;
     Ok(pairs.iter().map(pair_distance).sum())
 }
 
@@ -76,27 +70,11 @@ fn occurrance_map(i: &[u32]) -> HashMap<u32, u32> {
     map
 }
 
-fn part2(input: &str) -> Result<u32, Box<dyn Error>> {
-    let (left, right) = parse_lists(input)?;
-    let occ_map = occurrance_map(&right);
+fn part2(left: &[u32], right: &[u32]) -> Result<u32, Box<dyn Error>> {
+    let occ_map = occurrance_map(right);
 
-    let similarity_score = left
-        .into_iter()
-        .map(|l| l * occ_map.get(&l).unwrap_or(&0))
-        .sum();
+    let similarity_score = left.iter().map(|l| l * occ_map.get(l).unwrap_or(&0)).sum();
     Ok(similarity_score)
-}
-
-#[test]
-fn test_parse_pairs() {
-    let input = "3   4
-4   3
-2   5
-1   3
-3   9
-3   3";
-    let pairs = parse_pairs(input).expect("This should parse");
-    assert_eq!(pairs, vec![(1, 3), (2, 3), (3, 3), (3, 4), (3, 5), (4, 9)]);
 }
 
 #[test]
@@ -107,7 +85,8 @@ fn test_part1() {
 1   3
 3   9
 3   3";
-    let answer = part1(input).expect("No errors here please");
+    let (first, second) = parse_lists(input).expect("No errors here please");
+    let answer = part1(first, second).expect("No errors here please");
     assert_eq!(answer, 11);
 }
 
@@ -130,5 +109,6 @@ fn test_part2() {
 3   9
 3   3";
 
-    assert_eq!(part2(input).expect("No errors wanted"), 31);
+    let (first, second) = parse_lists(input).expect("No errors here please");
+    assert_eq!(part2(&first, &second).expect("No errors wanted"), 31);
 }
