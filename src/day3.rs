08ff532ceae33abf0a7086d1e954d8e45de46b6a
Matthew Walton <matthew@mathw.me.uk>
2024-12-03 17:18:52 +0000

    Day 3, Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -7,18 +7,31 @@ pub fn run() -> Result<(), Box<dyn Error>> {
     let input = include_str!("inputs/day3.txt");
 
     let part1 = part1(input)?;
-
     println!("Part 1: The sum is {}", part1);
+
+    let part2 = part2(input)?;
+    println!("Part 2: The sum is {}", part2);
 
     Ok(())
 }
 
 lazy_static! {
     static ref MUL_REGEX: Regex = Regex::new("mul\\((\\d{1,3}),(\\d{1,3})\\)").unwrap();
+    static ref INSTRUCTIONS_REGEX: Regex =
+        Regex::new("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)").unwrap();
 }
 
 fn part1(input: &str) -> Result<u32, Box<dyn Error>> {
     Ok(extract_mul_instructions(input)
+        .into_iter()
+        .map(evaluate_mul_instruction)
+        .collect::<Result<Vec<u32>, _>>()?
+        .into_iter()
+        .sum())
+}
+
+fn part2(input: &str) -> Result<u32, Box<dyn Error>> {
+    Ok(extract_enabled_muls(input)
         .into_iter()
         .map(evaluate_mul_instruction)
         .collect::<Result<Vec<u32>, _>>()?
@@ -33,6 +46,26 @@ fn extract_mul_instructions(input: &str) -> Vec<&str> {
     while let Some(m) = MUL_REGEX.find_at(input, start_pos) {
         start_pos = m.start() + 1;
         found.push(m.as_str())
+    }
+
+    found
+}
+
+fn extract_enabled_muls(input: &str) -> Vec<&str> {
+    let mut start_pos = 0;
+    let mut found = Vec::new();
+    let mut enabled = true;
+
+    while let Some(m) = INSTRUCTIONS_REGEX.find_at(input, start_pos) {
+        start_pos = m.start() + 1;
+
+        if enabled && m.as_str().starts_with("mul") {
+            found.push(m.as_str())
+        } else if enabled && m.as_str() == "don't()" {
+            enabled = false;
+        } else if !enabled && m.as_str() == "do()" {
+            enabled = true;
+        }
     }
 
     found
@@ -54,6 +87,9 @@ fn evaluate_mul_instruction(instruction: &str) -> Result<u32, Box<dyn Error>> {
 
 #[cfg(test)]
 const TEST_INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+#[cfg(test)]
+const TEST_INPUT_PART2: &str =
+    "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
 #[test]
 fn test_part1_sample_extract() {
@@ -73,4 +109,9 @@ fn test_evaluation() {
 #[test]
 fn test_part1() {
     assert_eq!(part1(TEST_INPUT).unwrap(), 161);
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(part2(TEST_INPUT_PART2).unwrap(), 48);
 }
